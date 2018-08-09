30718dd935c60fd06b996f756e30d189d1d1760f
Lindsey Maddox <lindsey@arcadiapower.com>
2018-08-09 16:22:32 -0400

    cleanup folders and put examples under assignment folder

---
+++ assignments/character_count/lindsey_attempt.py
@@ -0,0 +1,33 @@
+import operator
+def character_count(colors_arr):
+  max_char_list = []
+  for x in range(0, len(colors_arr)):
+    color_hash = get_color_hash(colors_arr[x])
+    max_char_list.append(color_hash)
+  print(max_char_list)
+  get_max_overall(max_char_list)
+
+
+def get_max_overall(list):
+  char_max = 0
+  max_char = []
+  for x in range(0,len(list)):
+      if list[x].values()[0] > char_max:
+        char_max = list[x].values()[0]
+  for x in range(0,len(list)):
+    if list[x].values()[0] == char_max
+      max_char.append(list[x])
+  print max_char
+
+def get_color_hash(color):
+  color_ct = {}
+  for x in range(0,len(color)):
+    if color[x] in color_ct.keys():
+      color_ct[color[x]] += 1
+    else:
+      color_ct[color[x]] = 1
+  print(color_ct)
+  key, value = max(color_ct.iteritems(), key=lambda x:x[1])
+  return {key: value}
+
+character_count(["red", "purple", "yellow"])
