0240f9334ea10692d39f8bef7ea7857ef8747554
Lindsey Maddox <lindsey@arcadiapower.com>
2018-08-10 16:28:02 -0400

    fcc sum numbers algorithm

---
+++ assignments/number_sums/lindsey_example.rb
@@ -0,0 +1,7 @@
+# We'll pass you an array of two numbers.
+# Return the sum of those two numbers
+# plus the sum of all the numbers between them.
+
+def method(numbers)
+  (numbers[0]..numbers[1]).to_a.reduce(:+)
+end
